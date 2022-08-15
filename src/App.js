14b391bbc4ec97a8bbcac987ef1be00367fe62d9
Abir Santra <santraabir8@gmail.com>
2022-08-15 19:40:29 +0530

    Add axios and react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import logo from "./images/logo black.png";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to Polaroid!</h1>
-      <img src={logo} alt="logo" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
