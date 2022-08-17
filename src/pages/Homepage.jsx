536352fd1df0175f23726245bbec5dd0e2723f93
Abir Santra <santraabir8@gmail.com>
2022-08-17 20:57:22 +0530

    homepage feed header designed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import "../styles/Homepage.css";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="home">
+      <div className="container home__container">
+        {/* Profile Section */}
+        <div className="home__profile--section">Profile</div>
+        {/* Feed Section */}
+        <div className="home__feed--section">
+          <div className="home__feed--header">
+            <h1 className="home__feed--welcomeMsg">
+              Welcome <span>User</span>!
+            </h1>
+            <div className="home__feed--links">
+              <a href="/" className="home__feed--link">
+                Following
+              </a>
+              <a href="/" className="home__feed--link">
+                Popular
+              </a>
+              <a href="/" className="home__feed--link">
+                Newest
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
