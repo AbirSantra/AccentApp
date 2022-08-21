d3b2092e14a3dffa7f96f99d76fa45e4e5f96341
Abir Santra <santraabir8@gmail.com>
2022-08-21 19:56:18 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
+import { Routes, Route } from "react-router-dom";
+import Searchpage from "./pages/Searchpage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="search" element={<Searchpage />} />
+      </Routes>
     </div>
   );
 }
