1f58c180c640583b269f1e363780f93963265f67
Abir Santra <santraabir8@gmail.com>
2022-08-22 20:07:40 +0530

    designed searchbar

--- src/pages/Searchpage.jsx
+++ src/pages/Searchpage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Searchpage.css";
+import { FaSearch } from "react-icons/fa";
 
 const Searchpage = () => {
   return (
@@ -8,11 +9,24 @@ const Searchpage = () => {
         {/* Search Bar */}
         <div className="searchpage__searchbar--container">
           <h1 className="searchbar__heading">Accent - Search</h1>
-          <p className="searchbar__desc">
-            Search for Creators, Posts or Topics
-          </p>
+          <p className="searchbar__desc">Search for Creators, Posts or Tags</p>
+          <div className="searchbar">
+            <input
+              type="text"
+              className="searchbar__input"
+              placeholder="Search creators, posts or tags"
+            />
+            <button className="searchbar__btn">
+              <FaSearch />
+            </button>
+          </div>
         </div>
         {/* Results Section */}
+        {/* <div className="searchpage__results">
+          {posts.map((post) => {
+            return <PostCard />;
+          })}
+        </div> */}
       </div>
     </div>
   );
