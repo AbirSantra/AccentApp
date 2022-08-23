dd26b0c1cb27ff1b8b8491c7c22d90eb64549b5a
Abir Santra <santraabir8@gmail.com>
2022-08-23 21:19:04 +0530

    designed profile page

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import "../styles/Profilepage.css";
 import coverImg from "../images/cover.jpg";
 import profileImg from "../images/profile1.jpg";
+import { IoPersonAddSharp } from "react-icons/io5";
 
 const Profilepage = () => {
   return (
     <div className="profilepage">
       <div className="profile__container container">
-        This is the profile page
+        {/* Profile Header */}
+
+        <div className="profile--header">
+          <div className="profile--banner">
+            <img src={coverImg} alt="profilebanner" />
+          </div>
+          <div className="profile--image">
+            <img src={profileImg} alt="profileimage" />
+          </div>
+          <div className="profile--infosection">
+            <div className="profile--userinfo">
+              <h1 className="profile--name">Abir Santra</h1>
+              <h2 className="profile--username">@drummingfreak</h2>
+              <p className="profile--headline">
+                FullStack Web Developer | Graphic Designer | Drummer | Gamer
+              </p>
+            </div>
+            <button className="primary-btn profile--followbtn">
+              <IoPersonAddSharp />
+              Follow
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
