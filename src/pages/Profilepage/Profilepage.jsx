0deb9f739fa795e4d834216de9905232ef5962f3
Abir Santra <santraabir8@gmail.com>
2022-08-24 21:48:43 +0530

    add profile stats

--- src/pages/Profilepage/Profilepage.jsx
+++ src/pages/Profilepage/Profilepage.jsx
@@ -14,21 +14,43 @@ const Profilepage = () => {
           <div className="profile--banner">
             <img src={coverImg} alt="profilebanner" />
           </div>
-          <div className="profile--image">
-            <img src={profileImg} alt="profileimage" />
-          </div>
-          <div className="profile--infosection">
-            <div className="profile--userinfo">
-              <h1 className="profile--name">Abir Santra</h1>
-              <h2 className="profile--username">@drummingfreak</h2>
-              <p className="profile--headline">
-                FullStack Web Developer | Graphic Designer | Drummer | Gamer
-              </p>
+          <div className="profile--image--section">
+            <div className="profile--image">
+              <img src={profileImg} alt="profileimage" />
             </div>
             <button className="primary-btn profile--followbtn">
               <IoPersonAddSharp />
               Follow
             </button>
+          </div>
+          <div className="profile--userinfo">
+            <h1 className="profile--name">Abir Santra</h1>
+            <h2 className="profile--username">@drummingfreak</h2>
+            <p className="profile--headline">
+              FullStack Web Developer | Graphic Designer | Drummer | Gamer
+            </p>
+            <div className="profile--user--stats">
+              <div className="profile--stat">
+                <p className="profile--stat--num">123</p>
+                <p className="profile--stat--label">Posts</p>
+              </div>
+              <div className="profile--stat">
+                <p className="profile--stat--num">1.2k</p>
+                <p className="profile--stat--label">Likes</p>
+              </div>
+              <div className="profile--stat">
+                <p className="profile--stat--num">23</p>
+                <p className="profile--stat--label">Accents</p>
+              </div>
+              <div className="profile--stat">
+                <p className="profile--stat--num">1.2k</p>
+                <p className="profile--stat--label">Followers</p>
+              </div>
+              <div className="profile--stat">
+                <p className="profile--stat--num">1.2k</p>
+                <p className="profile--stat--label">Following</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
