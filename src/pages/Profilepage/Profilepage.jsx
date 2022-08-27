020e033dc66dc72daab3254d8b62ae6917e19d51
Abir Santra <santraabir8@gmail.com>
2022-08-27 21:01:33 +0530

    Refactor and redesign profile page

--- src/pages/Profilepage/Profilepage.jsx
+++ src/pages/Profilepage/Profilepage.jsx
@@ -2,54 +2,64 @@ import React from "react";
 import "./Profilepage.css";
 import coverImg from "../../images/cover.jpg";
 import profileImg from "../../images/profile1.jpg";
-import { IoPersonAddSharp } from "react-icons/io5";
+// import { IoPersonAddSharp } from "react-icons/io5";
 
 const Profilepage = () => {
   return (
     <div className="profilepage">
       <div className="profile__container container">
-        {/* Profile Header */}
-
-        <div className="profile--header">
-          <div className="profile--banner">
-            <img src={coverImg} alt="profilebanner" />
+        {/* Profile Images Section */}
+        <div className="profile--images--section">
+          <div className="profile--images--cover--container">
+            <img
+              src={coverImg}
+              alt="profile cover"
+              className="profile--images--cover"
+            />
           </div>
-          <div className="profile--image--section">
-            <div className="profile--image">
-              <img src={profileImg} alt="profileimage" />
-            </div>
-            <button className="primary-btn profile--followbtn">
-              <IoPersonAddSharp />
+          <img
+            src={profileImg}
+            alt="profile display"
+            className="profile--images--photo"
+          />
+        </div>
+        {/* Profile Info Section */}
+        <div className="profile--info--section">
+          <h1 className="profile--info--name">Abir Santra</h1>
+          <p className="profile--info--username">@drummingfreak</p>
+          <p className="profile--info--headline">
+            MERN Stack Developer | Graphics Designer | Drummer | Gamer
+          </p>
+          {/* Buttons */}
+          <div className="profile--actions">
+            <button className="primary-btn profile--actions--btn">
               Follow
             </button>
+            <button className="secondary-btn profile--actions--btn">
+              Message
+            </button>
           </div>
-          <div className="profile--userinfo">
-            <h1 className="profile--name">Abir Santra</h1>
-            <h2 className="profile--username">@drummingfreak</h2>
-            <p className="profile--headline">
-              FullStack Web Developer | Graphic Designer | Drummer | Gamer
-            </p>
-            <div className="profile--user--stats">
-              <div className="profile--stat">
-                <p className="profile--stat--num">123</p>
-                <p className="profile--stat--label">Posts</p>
-              </div>
-              <div className="profile--stat">
-                <p className="profile--stat--num">1.2k</p>
-                <p className="profile--stat--label">Likes</p>
-              </div>
-              <div className="profile--stat">
-                <p className="profile--stat--num">23</p>
-                <p className="profile--stat--label">Accents</p>
-              </div>
-              <div className="profile--stat">
-                <p className="profile--stat--num">1.2k</p>
-                <p className="profile--stat--label">Followers</p>
-              </div>
-              <div className="profile--stat">
-                <p className="profile--stat--num">1.2k</p>
-                <p className="profile--stat--label">Following</p>
-              </div>
+          {/* Profile Stats */}
+          <div className="profile--stats--section">
+            <div className="profile--stat">
+              <h2 className="profile--stat--num">1.2k</h2>
+              <p className="profile--stat--label">Posts</p>
+            </div>
+            <div className="profile--stat">
+              <h2 className="profile--stat--num">1.2k</h2>
+              <p className="profile--stat--label">Followers</p>
+            </div>
+            <div className="profile--stat">
+              <h2 className="profile--stat--num">1.2k</h2>
+              <p className="profile--stat--label">Following</p>
+            </div>
+            <div className="profile--stat">
+              <h2 className="profile--stat--num">1.2k</h2>
+              <p className="profile--stat--label">Likes</p>
+            </div>
+            <div className="profile--stat">
+              <h2 className="profile--stat--num">1.2k</h2>
+              <p className="profile--stat--label">Accents</p>
             </div>
           </div>
         </div>
