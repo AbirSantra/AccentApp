f3df3d8ecb1a13b6c6405cd81bf02b9f6cec5d7d
Abir Santra <santraabir8@gmail.com>
2022-09-28 20:33:15 +0530

    added user search

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -7,6 +7,9 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 // Api call for getting a user
 export const getUser = (id) => API.get(`/user/${id}`);
 
+// Api call for getting users through search
+export const getUserSearch = (query) => API.get(`/user/search/${query}`);
+
 // Api call for getting a users followers
 export const getUserFollowers = (id) => API.get(`/user/${id}/followers`);
 
@@ -18,8 +21,8 @@ export const updateUser = (id, formData) => API.put(`/user/${id}`, formData);
 
 // Api call for following a user
 export const followUser = (id, currentUserId) =>
-  API.put(`/user/${id}/follow`, { currentUserId: currentUserId });
+	API.put(`/user/${id}/follow`, { currentUserId: currentUserId });
 
 // Api call for unfollowing a user
 export const unfollowUser = (id, currentUserId) =>
-  API.put(`/user/${id}/unfollow`, { currentUserId: currentUserId });
+	API.put(`/user/${id}/unfollow`, { currentUserId: currentUserId });
--- src/pages/Searchpage/Searchpage.jsx
+++ src/pages/Searchpage/Searchpage.jsx
@@ -1,35 +1,86 @@
 import React from "react";
 import "./Searchpage.css";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { getUserSearch } from "../../api/UserApi";
+import UserCard from "../../components/UserCard/UserCard";
 
 const Searchpage = () => {
-  return (
-    <div className="searchpage">
-      <div className="searchpage__container container">
-        {/* Search Bar */}
-        <div className="searchpage__searchbar--container">
-          <h1 className="searchbar__heading">Accent - Search</h1>
-          <p className="searchbar__desc">Find Creators across the globe</p>
-          <div className="searchbar">
-            <input
-              type="text"
-              className="searchbar__input"
-              placeholder="Search creators"
-            />
-            <button className="searchbar__btn">
-              <FaSearch />
-            </button>
-          </div>
-        </div>
-        {/* Results Section */}
-        {/* <div className="searchpage__results">
-          {posts.map((post) => {
-            return <PostCard />;
-          })}
-        </div> */}
-      </div>
-    </div>
-  );
+	// to store the search query
+	const [query, setQuery] = useState("");
+
+	// To store the search results
+	const [results, setResults] = useState(null);
+
+	// To store the error
+	const [error, setError] = useState(null);
+
+	// to remove whitespaces from query
+	const removeWhiteSpace = (str) => {
+		return /\s/g.test(str);
+	};
+
+	// to handle the query change
+	const handleQueryChange = (e) => {
+		if (removeWhiteSpace(e.target.value)) {
+			return;
+		} else {
+			setQuery(e.target.value);
+		}
+	};
+
+	// to handle search
+	const handleSearch = async (e) => {
+		e.preventDefault();
+		setResults(null);
+		setError(null);
+
+		if (query !== "") {
+			try {
+				const res = await getUserSearch(query);
+				setResults(res.data);
+			} catch (error) {
+				setError(
+					"There was a problem getting your results. Please try with a different search term. Your search query should contain only one word"
+				);
+			}
+		}
+	};
+
+	return (
+		<div className="searchpage">
+			<div className="searchpage__container container">
+				{/* Search Bar */}
+				<div className="searchpage__searchbar--container">
+					<h1 className="searchbar__heading">Accent - Search</h1>
+					<p className="searchbar__desc">Find Creators across the globe</p>
+					<div className="searchbar">
+						<input
+							type="text"
+							className="searchbar__input"
+							placeholder="Search username or firstname"
+							value={query}
+							onChange={handleQueryChange}
+						/>
+						<button className="searchbar__btn" onClick={handleSearch}>
+							<FaSearch />
+						</button>
+					</div>
+				</div>
+				{/* Error */}
+				{error && <p>{error}</p>}
+
+				{/* Results Section */}
+				{results && (
+					<div className="searchpage__results">
+						{results.map((user) => (
+							<UserCard user={user} key={user._id} />
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Searchpage;
