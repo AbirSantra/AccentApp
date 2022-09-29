d13a644b4f1f6104a0f9f21ce658e29b5ae4e178
Abir Santra <santraabir8@gmail.com>
2022-09-29 20:36:28 +0530

    added 404 page

--- src/App.js
+++ src/App.js
@@ -9,52 +9,54 @@ import { useSelector } from "react-redux";
 import Uploadpage from "./pages/Uploadpage/Uploadpage";
 import Savedpage from "./pages/Savedpage/Savedpage";
 import Postpage from "./pages/Postpage/Postpage";
+import NotFoundpage from "./pages/NotFoundpage/NotFoundpage";
 
 function App() {
-  const user = useSelector((state) => state.auth.authData);
-  // const user = JSON.parse(localStorage.getItem("profile"));
+	const user = useSelector((state) => state.auth.authData);
+	// const user = JSON.parse(localStorage.getItem("profile"));
 
-  /* Navbar is displayed on every page. Auth page will be displayed only if user is NOT logged in or they will be redirected to the home page. All other pages will be displayed if user IS logged in. Else, they will be redirected to the Auth page. */
+	/* Navbar is displayed on every page. Auth page will be displayed only if user is NOT logged in or they will be redirected to the home page. All other pages will be displayed if user IS logged in. Else, they will be redirected to the Auth page. */
 
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
-        />
-        <Route
-          path="/home"
-          element={user ? <Homepage /> : <Navigate to="../auth" />}
-        />
-        <Route
-          path="/auth"
-          element={user ? <Navigate to="../home" /> : <Authpage />}
-        />
-        <Route
-          path="/search"
-          element={user ? <Searchpage /> : <Navigate to="../auth" />}
-        />
-        <Route
-          path="/saved"
-          element={user ? <Savedpage /> : <Navigate to="../auth" />}
-        />
-        <Route
-          path="/profile/:id"
-          element={user ? <Profilepage /> : <Navigate to="../auth" />}
-        />
-        <Route
-          path="/post/:id"
-          element={user ? <Postpage /> : <Navigate to="../auth" />}
-        />
-        <Route
-          path="/upload"
-          element={user ? <Uploadpage /> : <Navigate to="../auth" />}
-        />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+				/>
+				<Route
+					path="/home"
+					element={user ? <Homepage /> : <Navigate to="../auth" />}
+				/>
+				<Route
+					path="/auth"
+					element={user ? <Navigate to="../home" /> : <Authpage />}
+				/>
+				<Route
+					path="/search"
+					element={user ? <Searchpage /> : <Navigate to="../auth" />}
+				/>
+				<Route
+					path="/saved"
+					element={user ? <Savedpage /> : <Navigate to="../auth" />}
+				/>
+				<Route
+					path="/profile/:id"
+					element={user ? <Profilepage /> : <Navigate to="../auth" />}
+				/>
+				<Route
+					path="/post/:id"
+					element={user ? <Postpage /> : <Navigate to="../auth" />}
+				/>
+				<Route
+					path="/upload"
+					element={user ? <Uploadpage /> : <Navigate to="../auth" />}
+				/>
+				<Route path="*" element={<NotFoundpage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
