7d4b11bf04708263e9cc776d6c85caa4593bb58c
Abir Santra <santraabir8@gmail.com>
2022-12-29 20:35:03 +0530

    added common axios instance

--- src/api/PostApi.js
+++ src/api/PostApi.js
@@ -1,18 +1,6 @@
 //! This file contains all the API calls related to Posts
 
-import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000" });
-
-API.interceptors.request.use((req) => {
-	if (localStorage.getItem("profile")) {
-		req.headers.Authorization = `Bearer ${
-			JSON.parse(localStorage.getItem("profile")).token
-		}`;
-	}
-
-	return req;
-});
+import API from "./axiosApi";
 
 // Api call for creating a post
 export const uploadPost = (formData) => API.post("post/", formData);
--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,18 +1,6 @@
 //! This file contains all the API calls related to users
 
-import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000" });
-
-API.interceptors.request.use((req) => {
-	if (localStorage.getItem("profile")) {
-		req.headers.Authorization = `Bearer ${
-			JSON.parse(localStorage.getItem("profile")).token
-		}`;
-	}
-
-	return req;
-});
+import API from "./axiosApi";
 
 // Api call for getting a user
 export const getUser = (id) => API.get(`/user/${id}`);
