267072222f5b6df1a01e76e742f10dc8719444c6
Abir Santra <santraabir8@gmail.com>
2022-12-29 20:34:12 +0530

    added axios configs

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -2,7 +2,11 @@
 
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+	baseURL: "http://localhost:5000",
+	withCredentials: true,
+	headers: { "Content-Type": "application/json" },
+});
 
 // Api call for user login
 export const logIn = (formData) => API.post("auth/login", formData);
